1efee92229b9ea75c67ac0d46d153aac1c5b4c67
Asim Azhar <asimali_5@hotmail.com>
2017-10-27 12:17:27 +0500

    Loops file

--- PycharmProjects/test/testclone.py
+++ PycharmProjects/test/testclone.py
@@ -1,1 +1,24 @@
-print ("Hello World")
+#print ("Hello World")
+## Calculating Mean
+import statistics
+
+
+var= {2,2,2,2,3,44,44,44}
+var1= {1,2,3,4,4,4,5,6}
+var2=4
+var3=10/var2
+print (statistics.mean(var))
+#print (statistics.median(var1))
+#print (statistics.mode({'dog','cat','cat','rat'}))
+
+if var3<100:
+    print ("first if passed")
+    if var==2:
+        print("second if passed")
+
+        print (statistics.variance({1,2,3,4,4,4,5,6}))
+    else:
+        print("else passed")
+
+        print ("no data")
+
