6942c7adec79570e3197737b581e87ce83293958
Asim Azhar <asimali_5@hotmail.com>
2017-11-14 15:41:02 +0500

    Reading Files

---
+++ PycharmProjects/Loops/ReadingFile.py
@@ -0,0 +1,28 @@
+# Reading Files
+ar=[]
+b=False
+counter=0
+with open('bc.txt')as file_object:
+ contents=file_object.read()
+ for word in contents.split():
+    if word in ar:
+        print(ar)
+        continue
+    else:
+     counter=0
+     fl=len(contents.split())
+    #print(fl)
+     for word2 in contents.split():
+       if word==word2:
+        counter=counter+1
+     if word not in ar:
+      ar.append(word)
+     print('word repeated ', word, counter)
+
+
+
+'''
+with open('c:/test/bc.txt')as file_object:
+    content = file_object.read()
+    print(content)
+'''
---
+++ PycharmProjects/Loops/check.py
@@ -0,0 +1,46 @@
+
+
+#def match_ends(words):
+
+''' ar2=[]
+nums=[1,2,2,3]
+for a in range(len(nums)):
+    b=nums[a]
+    print('value of b',b)
+    for s in nums:
+     if b not in ar2:
+        ar2.append(nums[a])
+print(ar2)
+'''
+
+
+
+
+
+
+
+
+
+
+'''
+list1=['aa','cc','kk']
+list2=['bb','dd','aa']
+list3=list1[:]
+a=0
+for a in range(len(list2)):
+  print('value of a',a)
+for b in list2:
+  print('value of b',b)
+'''
+
+''' for e in range(d):
+        ar.insert(0,ar2[e])
+    print(d)
+    print(ar2)
+    print(ar)'''
+
+    # +++your code here+++
+
+#           test(match_ends(['', 'x', 'xy', 'xyx', 'xx']), 2)
+ #            test(match_ends(['aaa', 'be', 'abc', 'hello']), 1)
+#  return
