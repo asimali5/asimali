135fe915833b5c178b79b58f56826c2f3c3fbc16
Asim Azhar <asimali_5@hotmail.com>
2017-11-17 12:19:20 +0500

    Reading Files & print Words

--- PycharmProjects/Loops/printwords.py
+++ PycharmProjects/Loops/printwords.py
@@ -1,4 +1,5 @@
-def print_word():
+
+def print_words(n):
     ar=[]
     ar3={}
     with open('bc.txt')as file_object:
@@ -10,22 +11,25 @@ def print_word():
         else:
          counter=0
          fl=len(contents.split())
-        #print(fl)
          for word2 in contents.split():
            if word.lower()==word2.lower():
             counter=counter+1
          if word not in ar:
           ar.append(word)
-     #    print('word repeated ', word, counter)
+         #print('word repeated ', word, counter)
          ar3.update({word.lower():counter})#code append on 11-16-2017 wednesday#
-         #print(ar3)
-
-
     a3=sorted(ar3.items()) # Printing keys in sorted orders
-    g=len(ar3) # Input from method, variable define to check how many rows to print
+    g=n#len(ar3)#input variable from user
     f=0 # variable define to check how many rows to print
-    for k,v in a3:# Printing keys in sorted orders
-         print(k,v)# Printing keys in sorted orders
+    a1_sorted_keys = sorted(ar3, key=ar3.get, reverse=True)  # Printing in Reverse orders, it is working fine
+    for r in a1_sorted_keys:  # Printing in Reverse orders, it is working fine
+        for k, v in a3:
+            if f == g:
+             break
+            else:
+             print(r, ar3[r])  # Printing in Reverse orders, it is working fine
+             f=f+1
+             break
     print('*****************************')
 
-print_word()
+print_words(2)
