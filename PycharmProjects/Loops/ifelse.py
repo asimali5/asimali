d35b7e36790584b7bc8a2a2f4efee616c9c4ab69
asimali5 <asim.azharali@gmail.com>
2017-12-11 12:23:23 +0500

    Ifelse

---
+++ PycharmProjects/Loops/ifelse.py
@@ -0,0 +1,21 @@
+
+
+loan=30000
+age=21
+Married=True
+Location=['DHA','Clifton']
+if age >= 18 and age <= 21:
+ print("age is within limit")
+ if Married==True:
+  print("Person is married")
+  if Location !='Lyari':
+   print("Location is ok")
+   print("loan can be given")
+elif age >= 40:
+    print("Over age")
+    print("loan can not be disbursed")
+
+
+
+
+
