f1a2d9eae000125a0857ab612d395f7a795df834
asimali5 <asim.azharali@gmail.com>
2017-12-25 13:58:12 +0500

    Assignment 6.1

---
+++ PycharmProjects/Loops/assignment6.1.py
@@ -0,0 +1,22 @@
+##assignment 6.1
+'''
+ad_0={'fname':'ali','lname':'bilal','age':30,'city':'karachi'}
+print(ad_0['fname'])
+print(ad_0['lname'])
+print(ad_0['age'])
+print(ad_0['city'])
+'''
+
+
+## assignment 6.2
+'''
+ad_1={'ali':2,'bilal':4,'hamza':6}
+for a,b in ad_1.items():
+ print(a,b)
+'''
+ ## assignment 6.3
+ '''
+ ad_1 = {'python': 'programming language', 'anaconda': 'compiler'}
+ for a, b in ad_1.items():
+     print("\n",a, b)
+ '''
